99ce8613ce1cd58f771ed2919d9d52d61f92609a
Kyle44 <kyle44@comcast.net>
2015-04-02 00:51:27 -0400

    Create Tent.cpp

---
+++ Tent.cpp
@@ -0,0 +1,16 @@
+// Tent.cpp
+#include "Tent.h"
+
+Tent::Tent(): Residence(1, 2 , false)
+{
+}
+
+int Tent::PropertyValue() const
+{
+  return 0;
+}
+
+int Tent::NumWindows() const
+{
+  return 0;
+}
